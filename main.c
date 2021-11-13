debfbcb5b3f9251eb7c7cae1bd55f601a7857f83
Jim Morrissey <jpm9511@gmail.com>
2021-11-13 11:49:16 -0700

    Commit on local dev after commiting change to same line in origin/dev

--- main.c
+++ main.c
@@ -19,6 +19,7 @@ int main() {
     int two = 2;
     int ans = add2ints(one, two);
     printf("one + two = %d\n", ans);
+    printf("dev: Print some new stuff\n");
     ans = mult2ints(one, two);
     printf("one * two = %d, you dig?\n", ans);
     ans = sub2ints(one, two);
